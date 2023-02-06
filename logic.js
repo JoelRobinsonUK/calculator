fef956e18528323149abf19c85296295df4679ad
JoelRobinsonUK <49420265+JoelRobinsonUK@users.noreply.github.com>
2023-02-06 21:10:17 +0000

    Minor Fixes

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 const $buttons = document.querySelectorAll(".buttons div");
 
 $buttons.forEach((node) => {
-    node.addEventListener('click', (e => {
+    node.addEventListener('mousedown', (e => {
         // e.preventDefault();
         const value = node.innerText.trim();
         const $results = document.querySelector(".results");
