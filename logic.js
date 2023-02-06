fcb6cdde32dc8bc9dbc94b8cc4f0e491c286741e
JoelRobinsonUK <49420265+JoelRobinsonUK@users.noreply.github.com>
2023-02-06 20:53:34 +0000

    Logic Added
    +Added calculator logic
    - Removed Tailwind CSS
    + Added custom CSS

---
+++ logic.js
@@ -0,0 +1,28 @@
+const $buttons = document.querySelectorAll(".buttons div");
+
+$buttons.forEach((node) => {
+    node.addEventListener('mousedown', (e => {
+        e.preventDefault();
+        const value = node.innerText.trim();
+        const $results = document.querySelector(".results");
+
+        const resultText = $results.innerText.trim();
+
+        if(resultText == "0" || resultText == "undefined" || resultText == "Infinity"){
+            $results.innerText = "";
+        }
+
+        if(value == "="){
+            let solution = eval(resultText);
+            $results.innerText = solution;
+            return true;
+        }
+
+        if (value.toLowerCase() === "c") {
+            $results.innerText = "0";
+            return true;
+        }
+
+        $results.innerText += value;
+    }));
+});
