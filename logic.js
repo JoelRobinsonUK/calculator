5ff5e88050ba3fe5ae70bcdbef74064b2cec90e2
Joel Robinson <joelsgrobinson@gmail.com>
2023-02-20 12:52:11 +0000

    Responsiveness Fix
    + Set body to 100vh and main calculator element to 100% heights to keep all elements on longer mobile screens
    
    + Tried to rewrite the logic using jQuery, no luck.

--- logic.js
+++ logic.js
@@ -1,3 +1,27 @@
+// $(document).ready(() => {
+
+//     $(".buttons .btn").click(e => {
+
+//         e.preventDefault();
+
+//         const node = e.currentTarget;
+//         const value = node.innerText.trim();
+
+//         $(".results").text(resultText => {
+
+//             if(resultText == "0" || resultText == "undefined" || resultText == "Infinity"){
+//                 $(".results").text("");
+//             }
+
+//             $(".results").text(t => {
+//                 console.log(t + value);
+//             })
+//         });
+
+//     });
+// });
+
+
 const $buttons = document.querySelectorAll(".buttons div");
 
 $buttons.forEach((node) => {
@@ -10,11 +34,11 @@ $buttons.forEach((node) => {
 
         console.log(value);
 
-        if(resultText == "0" || resultText == "undefined" || resultText == "Infinity"){
+        if (resultText == "0" || resultText == "undefined" || resultText == "Infinity") {
             $results.innerText = "";
         }
 
-        if(value == "="){
+        if (value == "=") {
             let solution = eval(resultText);
             $results.innerText = solution;
             return true;
